097143e89b9a1100c37613e76a35deefafd988bd
Gowtham <76685953+gowtham949@users.noreply.github.com>
2024-04-01 23:13:31 +0530

    Delete Untitled-2.py

--- Untitled-2.py
+++
@@ -1,7 +0,0 @@
-def compound_interest(p,t,r):
-    A = P(1+R/100)**t
-    CI = A-P
-P = int(input("enter the principle amount:"))
-R = int(input("enter the interest rate:"))
-T = int(input("enter the time period:"))
-compound_interest(1000,12,2)
